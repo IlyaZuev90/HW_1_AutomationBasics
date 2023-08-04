7dff2930de07ed1391c09611ccf7a06bcc4840a7
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-08-04 05:21:49 +0400

    Added Intro part of the Task

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,18 @@
 package ru.netology;
 
+import ru.netology.service.CashbackHackService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int boundary = 1000;
+
+        if (amount < boundary) {
+            System.out.println("Необходимо докупить товаров на сумму: " + service.remain(amount));
+        } else {
+            System.out.println("Кэш-бэк зачислен на счёт покупателя");
+            System.out.println(service.remain(amount));
+        }
     }
 }

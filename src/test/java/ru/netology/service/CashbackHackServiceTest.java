9df41719b8245ecb131003482e435d75d56c9bbd
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-08-04 12:53:55 +0400

    Added solution for the part 2 of the Task - junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.service;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void shouldTestRemainWithEqualAmount() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void shouldTestRemainWithLesserAmount() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void shouldTestRemainWithHigherAmount() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1200;
+
+        int actual = service.remain(amount);
+        int expected = 800;
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void shouldTestRemainWithZeroAmount() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+}
